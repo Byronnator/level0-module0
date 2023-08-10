aa616590c98b76a0af9d7d6678bfdd60a40159d5
root <byronnatorshorts@gmail.com>
2023-08-10 01:28:16 +0000

    I did 1 c and I did 2 a and started 2 b

--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -21,21 +21,26 @@ if __name__ == '__main__':
     colors = ('red', 'blue', 'green', 'yellow', 'orange')
     
     # Make a new turtle
-    
+    bob = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    bob.shape('turtle')
     # Set the turtle speed to max (0)
-    
+    bob.speed(0)
     # Set the turtle width to 1
-    
+    bob.width(1)
     # Create a variable to hold the number of sides in a pentagon
-    
+    sides = 5
     # Create a variable to be the angle of 360 divided by the sides variable
-    
+    space = 360/sides
     # Use a for loop to repeat ALL the following lines of code 360 times. 
-        
+    for i in range(360):
         # If the loop variable (i) is equal to 100, set the turtle width to 2
-        
+        if i == 100:
+            bob.width(2)
+        if i == 200:
+            bob.width(3)
+        bob.pencolor(get_next_color(i))
+        bob.foward(i)
         # If the loop variable (i) is equal to 200, set the turtle width to 3
         
         # Use the get_next_color function to set the turtle pencolor,
