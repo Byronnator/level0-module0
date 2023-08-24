954922c45d139ebe485c00cc459b5dabc97c2bbf
root <byronnatorshorts@gmail.com>
2023-08-24 01:24:06 +0000

    i did mad libs and have you coded and I am almost finished with quiz

--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     # Get the player to enter an adjective
     adjective= simpledialog.askstring(title= None, prompt = 'enter a adjective')
     # Get the player to enter a type of liquid
-    simpledialog.askstring(title= None, prompt = 'enter a liquid')
+    liquid= simpledialog.askstring(title=None, prompt = 'enter a liquid')
     # Get the player to enter a body part
-    simpledialog.askstring(title=None, prompt='enter a body part')
+    bodypart= simpledialog.askstring(title=None, prompt='enter a body part')
     # Get the player to enter a verb
-    simpledialog.askstring(title=None, prompt='enter a verb')
+    verb= simpledialog.askstring(title=None, prompt='enter a verb')
     # Get the player to enter a place
-    simpledialog.askstring(title=None, prompt='enter a place')
+    place= simpledialog.askstring(title=None, prompt='enter a place')
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
@@ -27,17 +27,17 @@ if __name__ == '__main__':
     #       other parts of the story.
 
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
+        "Piranhas are more "+adjective+" during the day, so cross the river at\n"
+        "night. Piranhas are attracted to fresh "+liquid+" and will most\n"
+        "likely take a bite out of your "+bodypart+" if you "+verb+". Whatever\n"
         "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        "back to the "+place+". Good luck!"
     )
 
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
     # story look better in the pop-up
-
+    messagebox.showinfo(title=None, message= story)
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
 
--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,15 +1,24 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__=='__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
-
+    score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
-
+    spanish = simpledialog.askstring(title= None, prompt = 'What country has the most spanish speakers')
+    if spanish == 'Mexico':
+        score = score + 2
+    else:
+        score = score - 1
+    mostspeakers = simpledialog.askstring(title= None, prompt = 'What language had the most amount of speakers')
+    if mostspeakers == 'english':
+        score += 2
+    else:
+        score -= 1
     #      // 2.  Ask the user a question 
     
     #      // 3.  Use an if statement to check if their answer is correct
